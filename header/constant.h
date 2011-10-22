2375229f32152ef4bf2f9f2908f68fb4ef6025bc
xavier.schepler@gmail.com <xavier.schepler@gmail.com@3c67194d-c54d-67c7-a969-05fdd0412097>
2011-10-22 20:49:19 +0000

    Nettoyage, restructuration. Ajout probable de quelques bugs que je corrigerais demain. J'avance sur l'interface CLI pour l'instant.

---
+++ header/constant.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANT_H
+  #define CONSTANT_H
+
+#define UNEVALUATED 	2  	// valeur de travail; ne doit pas apparaître en début ou fin d'itération 
+#define TRANSFERT	1
+#define SWAP		2
+#define ROTATION	3
+#define NO_JOB		1	// pas de tâche disponible pour le calcul du voisinage 
+#define NO_AGT 		2	// pas de d'agent disponible pour le calcul du voisinage 
+
+
+#endif
