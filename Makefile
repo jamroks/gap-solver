6ff7dd0b8a93b7dd43de27f5a1585ba345e2dbb4
xavier.schepler@gmail.com <xavier.schepler@gmail.com@3c67194d-c54d-67c7-a969-05fdd0412097>
2011-10-23 09:30:00 +0000

    Correction d'un bogue lié à un passage de tableau à 2 dimensions à une fonction. Petits rappels sur l'arithmétique des pointeurs...

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC=gcc
 
 all: solver
-solver : cli_parser.o input_orlibrary.o memory_gap_instance.o memory_gap_solution.o
-	$(CC) gap-solver.c cli_parser.o input_orlibrary.o memory_gap_instance.o memory_gap_solution.o
+solver : cli_parser input_orlibrary memory_gap_instance memory_gap_solution solver_solution_0
+	$(CC) gap_solver.c cli_parser.o input_orlibrary.o memory_gap_instance.o memory_gap_solution.o
 cli_parser : cli/parser.c
 	$(CC) -c cli/parser.c -o cli_parser.o
 input_orlibrary : input/orlibrary.c
@@ -11,5 +11,8 @@ memory_gap_instance :
 	$(CC) -c memory/gap_instance.c -o memory_gap_instance.o
 memory_gap_solution : 
 	$(CC) -c memory/gap_solution.c -o memory_gap_solution.o
-#clean :
+solver_solution_0:
+	$(CC) -c solver/solution_0.c -o solver_solution_0.o
+clean :
+	rm *\.o
 
--- solver/solution_0.c
+++ solver/solution_0.c
@@ -17,6 +17,9 @@ along with gap_solver. If not, see <http://www.gnu.org/licenses/>.
 
 #include "../header/common.h"
 
+static void
+_update_mean (float *, float *, int, int);
+
 short
 search_solution_0 (t_gap_instance * instance, t_gap_solution * solution)
 {
@@ -33,7 +36,7 @@ search_solution_0 (t_gap_instance * instance, t_gap_solution * solution)
   float mean[instance->job_count]; // mean of job capacity cost on each agent
   for (assigned = 0 ; assigned < instance->job_count ; assigned ++)
     {
-      _update_mean (mean, ratio, instance->agent_count, instance->job_count) ;
+      _update_mean (mean, ratio[0], instance->agent_count, instance->job_count) ;
       for (max_mean_job = job = 0 ; job < instance->job_count ; job ++)
         if (mean[job] > mean[max_mean_job])
           max_mean_job = job ;
@@ -52,8 +55,8 @@ search_solution_0 (t_gap_instance * instance, t_gap_solution * solution)
 
 static void
 _update_mean (
-  float ** mean,
-  float ** ratio,
+  float * mean,
+  float * ratio,
   int agent_count,
   int job_count
 )
@@ -64,7 +67,7 @@ _update_mean (
     {
       for (agent = 0 ; agent < agent_count ; agent ++)
         {
-          sum += ratio[agent][job] ;
+          sum += ratio[agent_count * agent + job] ;
         }
       mean[job] = sum / job_count ;
     }
