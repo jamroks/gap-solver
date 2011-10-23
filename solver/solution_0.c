56ca048ab92f77bf4baae206871ab12c77c02ae9
xavier.schepler@gmail.com <xavier.schepler@gmail.com@3c67194d-c54d-67c7-a969-05fdd0412097>
2011-10-23 08:47:24 +0000

    Bon j'ai fini d'implanter l'algorithme de recherche de la première solution. J'ai ajouté une license sur notre code. Un café, je teste et débogue mes modifications puis c'est fini pour aujourd'hui.

--- solver/solution_0.c
+++ solver/solution_0.c
@@ -20,34 +20,52 @@ along with gap_solver. If not, see <http://www.gnu.org/licenses/>.
 short
 search_solution_0 (t_gap_instance * instance, t_gap_solution * solution)
 {
-  int assigned;
-  // (job capacity cost) / (agent capacity) ratios
-  float ratio[instance->agent_count][instance->job_count];
-  // mean of job capacity cost on each agent
-  float mean[instance->job_count];
-  // ratios initialization
+  float ratio[instance->agent_count][instance->job_count]; // (job capacity cost) / (agent capacity) ratios
   int agent, job;
-  for (agent = 0 ; agent < instance->agent_count ; agent ++)
+  for (agent = 0 ; agent < instance->agent_count ; agent ++) // ratios initialization
     {
       for (job = 0 ; job < instance->job_count ; job ++)
         {
           ratio[agent][job] = instance->cost[agent][job] / solution->capacity_left[agent];
         }
     }
-
-  for (assigned = 0 ; assigned < instance->job_count ; )
+  int assigned, max_mean_job, min_ratio_agent;
+  float mean[instance->job_count]; // mean of job capacity cost on each agent
+  for (assigned = 0 ; assigned < instance->job_count ; assigned ++)
     {
-      _update_mean (mean, ratio, instance) ;
+      _update_mean (mean, ratio, instance->agent_count, instance->job_count) ;
+      for (max_mean_job = job = 0 ; job < instance->job_count ; job ++)
+        if (mean[job] > mean[max_mean_job])
+          max_mean_job = job ;
+      for (min_ratio_agent = agent = 0; agent < instance->agent_count ; agent ++)
+        if (ratio[agent][max_mean_job] < ratio[min_ratio_agent][max_mean_job])
+          min_ratio_agent = agent ;
+      if (solution->capacity_left[agent] < instance->cost[min_ratio_agent][max_mean_job])
+        return 0 ;
+      solution->capacity_left[agent] -= instance->cost[min_ratio_agent][max_mean_job] ;
+      solution->assignment[min_ratio_agent][max_mean_job] = 1 ;
+      for (job = 0; job < instance->job_count ; job ++)
+        ratio[min_ratio_agent][job] = instance->cost[min_ratio_agent][job] / solution->capacity_left[min_ratio_agent];
     }
+  return 1;
 }
 
 static void
 _update_mean (
   float ** mean,
-  t_gap_instance * instance,
-  t_gap_solution * solution,
-  t_gap_instance * instance
+  float ** ratio,
+  int agent_count,
+  int job_count
 )
 {
-
+  int job, agent ;
+  float sum ;
+  for (job = 0 ; job < job_count ; job ++)
+    {
+      for (agent = 0 ; agent < agent_count ; agent ++)
+        {
+          sum += ratio[agent][job] ;
+        }
+      mean[job] = sum / job_count ;
+    }
 }
