7e7ba71c19aa6ce912c450306b923ebeab3bc0ec
xavier.schepler@gmail.com <xavier.schepler@gmail.com@3c67194d-c54d-67c7-a969-05fdd0412097>
2011-10-23 10:11:31 +0000

    Réécriture des fonctions d'affichage de la solution, débogage continue :)

--- trunk/Makefile
+++ trunk/Makefile
@@ -1,18 +1,31 @@
 CC=gcc
 
 all: solver
-solver : cli_parser input_orlibrary memory_gap_instance memory_gap_solution solver_solution_0
-	$(CC) gap_solver.c cli_parser.o input_orlibrary.o memory_gap_instance.o memory_gap_solution.o
+
+solver : cli_parser cli_print_result input_orlibrary\
+ memory_gap_instance memory_gap_solution solver_solution_0
+	$(CC) gap_solver.c cli_parser.o cli_print_result.o\
+ input_orlibrary.o memory_gap_instance.o memory_gap_solution.o\
+ solver_solution_0.o -o gap_solver
+
 cli_parser : cli/parser.c
 	$(CC) -c cli/parser.c -o cli_parser.o
+
+cli_print_result : cli/print_result.c
+	$(CC) -c cli/print_result.c -o cli_print_result.o
+
 input_orlibrary : input/orlibrary.c
 	$(CC) -c input/orlibrary.c -o input_orlibrary.o
+
 memory_gap_instance : 
 	$(CC) -c memory/gap_instance.c -o memory_gap_instance.o
+
 memory_gap_solution : 
 	$(CC) -c memory/gap_solution.c -o memory_gap_solution.o
+
 solver_solution_0:
 	$(CC) -c solver/solution_0.c -o solver_solution_0.o
+
 clean :
 	rm *\.o
 
---
+++ trunk/cli/print_result.c
@@ -0,0 +1,54 @@
+/*
+This file is part of gap_solver.
+
+gap_solver is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+gap_solver is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with gap_solver. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "../header/common.h"
+
+short 
+print_result (t_gap_instance *instance, t_gap_solution *solution)
+{
+  int valeur ;
+  int agent,job ;
+  int nb_line=0 ;
+  int assignments ;
+
+  printf ("%5d\n%5d\n", instance->agent_count, instance->job_count);
+
+  for (agent = 0 ; agent < instance->agent_count ; agent ++)
+    {
+      assignments=0 ;
+      for (job = 0 ; job < instance->job_count ; job++)
+        {
+          if (solution->assignment[agent][job] == 1)
+            {
+              assignments ++ ;
+            }
+        }
+      printf (
+        "\%5d %5d %5d\n", 
+        instance->capacity[agent],
+        solution->capacity_left[agent],
+        assignments
+      );
+      
+      for (job=0;job < instance->job_count;job++)
+          if (solution->assignment[agent][job] == 1)
+            printf ("%5d ", job);
+
+      printf ("\n") ;
+    };
+  return 0 ;
+}
