75c43cf81ef519c2c30542ed3bfaeeedeca1cdab
romain.maillard@free.fr <romain.maillard@free.fr@3c67194d-c54d-67c7-a969-05fdd0412097>
2011-10-24 21:47:49 +0000

    intégration dans le makefile

---
+++ trunk/utility/subtract_elt_from_list.c
@@ -0,0 +1,16 @@
+
+/* fonction pour enlever un élément d'une liste */
+/* ============================ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+/* ============================ */
+#include "../header/type.h"
+/* ============================ */
+void 
+subtract_elt_from_list(t_list list_of_values,t_elt agt_1) 
+{
+list_of_values.list[agt_1]=0 ;
+}
