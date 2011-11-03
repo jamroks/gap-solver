b1c62e0d1affc1d80c7f4e4e9265991d778932f0
xavier.schepler@gmail.com <xavier.schepler@gmail.com@3c67194d-c54d-67c7-a969-05fdd0412097>
2011-11-03 19:52:12 +0000

    Ajout d'une fonction d'application des changements

--- trunk/utility/solution.c
+++ trunk/utility/solution.c
@@ -17,6 +17,15 @@ along with gap_solver. If not, see <http://www.gnu.org/licenses/>.
 
 #include "../header/common.h"
 
+static t_gap_instance * _instance ;
+static t_gap_solution * _solution ;
+
+static void
+_job_add (t_agent agent, t_job job) ;
+
+static void
+_job_remove (t_agent agent, t_job job) ;
+
 void
 solution_apply_change (
   t_gap_instance * instance,
@@ -24,5 +33,55 @@ solution_apply_change (
   t_solution_change * change
 )
 {
-
+  _solution = solution ;
+  _instance = instance ;
+  solution->value += change->delta_value ;
+  switch (change->type)
+    {
+      case SOLUTION_CHANGE_TRANSFER:
+        _job_remove (          
+          change->contents.transfer.source,
+          change->contents.transfer.job
+        ) ;
+        _job_add (
+          change->contents.transfer.destination,
+          change->contents.transfer.job
+       ) ;
+        break ;
+      case SOLUTION_CHANGE_SWAP:
+        _job_remove (          
+          change->contents.swap.source,
+          change->contents.swap.source_swapped_job
+        ) ;
+        _job_add (
+          change->contents.swap.source,
+          change->contents.swap.destination_swapped_job
+       ) ;
+        _job_remove (          
+          change->contents.swap.destination,
+          change->contents.swap.destination_swapped_job
+        ) ;
+        _job_add (
+          change->contents.swap.destination,
+          change->contents.swap.source_swapped_job
+       ) ;
+        break ;
+    }
 }
+
+static void
+_job_add (t_agent agent, t_job job)
+{
+  _solution->assignment[agent][job] = 1 ;
+  _solution->capacity_left[agent] -= _instance->cost[agent][job] ;
+  add_job_to_job_list (_solution->ll_assignment[agent], job) ;
+}
+
+static void
+_job_remove (t_agent agent, t_job job)
+{
+  _solution->assignment[agent][job] = 0 ;
+  _solution->capacity_left[agent] += _instance->cost[agent][job] ;
+  remove_job_from_job_list (_solution->ll_assignment[agent], job) ;
+}
+
